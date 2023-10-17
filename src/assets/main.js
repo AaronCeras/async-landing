70d853fc480a5f805838f8ef971593aabbb3806f
Aaron Ceras <aaron_dash_96@hotmail.com>
2023-10-17 13:30:06 -0500

    Subiendo todos los cambios realizados en el curso

---
+++ src/assets/main.js
@@ -0,0 +1,49 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC3aj05GEEyzdOqYM5FLSFeg&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c41b721cb9mshc534d435bd4333cp133b6cjsncd9a55f702d8',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+            <div class="group relative">
+                <div
+                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-gray-700">
+                        <span aria-hidden="true" class="absolute inset-0"></span>
+                        ${video.snippet.title}
+                    </h3>
+                </div> 
+            </div> 
+        `).slice(0, 4).join('')}
+        `;
+        content.innerHTML = view;
+    } catch(error) {
+        console.log(error);
+    }
+}) ();
